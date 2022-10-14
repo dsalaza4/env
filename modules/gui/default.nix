ca8a601b380da48e4196b1c89d028e9714c2907c
Daniel Salazar <podany270895@gmail.com>
2022-10-14 17:46:00 -0500

    feat: fonts
    - Use monospace for everything

--- modules/gui/default.nix
+++ modules/gui/default.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   services.xserver = {
     enable = true;
     layout = "us,latam";
@@ -11,5 +11,36 @@
       };
     };
     desktopManager.gnome.enable = true;
+  };
+
+  fonts = {
+    enableDefaultFonts = false;
+    fontDir.enable = true;
+    fonts = [
+      pkgs.noto-fonts-emoji
+      pkgs.twitter-color-emoji
+      pkgs.fira-code
+    ];
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        monospace = ["Fira Code"];
+        sansSerif = ["Fira Code"];
+        serif = ["Fira Code"];
+        emoji = [
+          "Twitter Color Emoji"
+          "Noto Color Emoji"
+        ];
+      };
+    };
+  };
+  home-manager.users.dsalazar = {
+    gtk = {
+      enable = true;
+      font = {
+        name = "monospace";
+        size = 12;
+      };
+    };
   };
 }

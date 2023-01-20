72252f00df62c7be04e94d77b86b6a1c9e262674
Daniel Salazar <podany270895@gmail.com>
2023-01-20 15:42:03 -0500

    feat: screen features
    - Add xrandr for showing display info
    - Add wl-mirror for mirroring screens
    - Add a more generic configuration for when
    a HDMI cable is plugged in to the laptop

--- modules/gui/screens.nix
+++ modules/gui/screens.nix
@@ -1,3 +1,5 @@
+# To see display options, use xrandr
+# To create mirror displays, use wl-mirror <output>
 {pkgs, ...}: {
   home-manager.users.nixos = {
     services.kanshi = {
@@ -6,13 +8,15 @@
         home-studio.outputs = [
           {
             criteria = "eDP-1";
-            status = "disable";
+            status = "enable";
+            mode = "1920x1080@60Hz";
+            position = "0,0";
           }
           {
             criteria = "HDMI-A-1";
             status = "enable";
-            mode = "3840x2160@60Hz";
-            position = "0,0";
+            mode = "1920x1080@60Hz";
+            position = "1920,0";
           }
         ];
         home-workstation.outputs = [
@@ -39,6 +43,8 @@
     };
     home.packages = [
       pkgs.kanshi
+      pkgs.xorg.xrandr
+      pkgs.wl-mirror
     ];
   };
 }

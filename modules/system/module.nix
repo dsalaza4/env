1d1ba141c78dedec14470439d9ae7262b590b2e3
Daniel Salazar <dsalazar@fluidattacks.com>
2026-08-12 12:28:13 -0500

    refac: relax cleaning

--- modules/system/module.nix
+++ modules/system/module.nix
@@ -43,7 +43,8 @@
       RunAtLoad = false;
       StartCalendarInterval = [
         {
-          Hour = 13;
+          Weekday = 5;
+          Hour = 22;
           Minute = 0;
         }
       ];
